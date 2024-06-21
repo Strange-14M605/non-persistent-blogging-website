5eee8e3a2c05439eef099655e43a140a49a41ddc
Jova <jova16.2004@gmail.com>
2024-06-21 16:11:58 +0530

    NewPost, ViewPost and DeletePost integrated with no errors(EditPost to be included later)

--- index.js
+++ index.js
@@ -1,70 +1,12 @@
-// import express from "express"
-// import bodyParser from "body-parser"
-
-// const app=express();
-// const port=3000;
-
-// var title=[], tag = [], content = [];
-
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static("public"));
-
-// app.get("/",(req,res)=>{
-//     res.render("home.ejs",
-//         {
-//             title: title,
-//             tag: tag,
-//             content: content
-//         }
-//     );
-// })
-
-// app.get("/newpost",(req,res)=>{
-//     res.render("newpost.ejs");
-// })
-
-// app.post("/", (req,res)=>{
-//     content.push(req.body["content"]);
-//     title.push(req.body["title"]);
-//     var tag_str = Object.keys(req.body).find(key =>
-//         req.body[key] == "on");
-//     tag.push(tag_str);  
-//     res.redirect("/");
-// })
-
-
-
-// app.get("/view", (req, res)=>{
-//     var number= req.body["number"];
-//     console.log(number);
-//     res.render("view.ejs",
-//         {
-//             title: title[number],
-//             tag: tag[number],
-//             content: content[number]
-//         }
-//     );
-// })
-// app.delete("/delete", (req,res)=>{
-//     //remove from lists
-//     res.redirect("/");
-// })
-
-// app.listen(port, ()=>{
-//     console.log(`Serving running on port ${port}`);
-// })
-
 import express from "express"
 import bodyParser from "body-parser"
 
 const app= express();
 const port= 3000
 var title;
-//number of columns= 3
-//number of rows= adjustible (using push)
 
 let blogList= [];
-var empty;
+var func;
 
 //middleware
 app.use(bodyParser.urlencoded({extended: true}));
@@ -72,25 +14,36 @@ app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
     res.render("index.ejs",{
-        empty:empty,
+        func:func,
         blogList: blogList
     });
-})
-
-app.post("/",(req,res)=>{
-    empty=1;
-    // newArray= [req.body.title, req.body.content];
-    // blogList.push(newArray);
-    let newArray = [req.body.title, req.body.blogContent];
-    blogList.push(newArray);
-    console.log(blogList);
-    res.redirect("/");
 })
 
 app.get("/newPost", (req,res)=>{
     res.render("index.ejs",{
         newPost:"What do you want to write about today?"
     });
+})
+
+app.post("/",(req,res)=>{
+    func=1;
+    let newArray = [req.body.title, req.body.tag, req.body.blogContent];
+    blogList.push(newArray);
+    res.redirect("/");
+})
+
+app.post("/viewPost", (req,res)=>{
+    res.render("index.ejs",{
+        viewPost: 1,
+        blog: blogList[+req.body.blogNumber],        //note: the '+' symbol coverts the string blogNumber to integer to pass to arry in index.ejs!
+        blogNumber: +req.body.blogNumber
+    });
+})
+
+app.post("/deletePost", (req,res)=>{
+    blogList.splice(+req.body.blogNumber, 1);
+    console.log(blogList);
+    res.redirect("/");
 })
 
 app.listen(port, ()=> {
