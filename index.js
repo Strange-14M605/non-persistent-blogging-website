8dd44d62a969f940accb01cd8d69bda8921d3e62
Jova <jova16.2004@gmail.com>
2024-06-21 20:35:17 +0530

    final project commit(end)

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 import express from "express"
 import bodyParser from "body-parser"
+import axios from "axios"
+import config from './config.json' with { type: "json" };
 
 const app= express();
 const port= 3000
-var title;
+
+const API_URL="https://api.notion.com/v1/";
+// const configure = require('./config.json');
+const yourToken= config.API_TOKEN;
 
 let blogList= [];
+let title= [];
 var func;
 
 //middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req,res)=>{
-    res.render("index.ejs",{
-        func:func,
-        blogList: blogList
-    });
+const configure={
+    headers:{
+        Authorization: `Bearer ${yourToken}`,
+        "Notion-Version": "2022-06-28"
+    }
+};
+
+const response= await axios.post(`${API_URL}databases/f3905f63-6ea3-437f-a41c-ed5f939d9d5e/query`,{}, configure);
+for(let i=0;i<(response.data.results).length; i++){
+    title.push(response.data.results[i].properties.Name.title[0].plain_text);
+}
+
+app.get("/", async(req,res)=>{
+    try{
+        console.log(title);
+        res.render("index.ejs",
+            {
+                func:func,
+                blogList:blogList,
+                title:title
+            }
+        )
+    }catch(error){
+        console.log(`Error found: ${error.message}`);
+    }
 })
 
 app.get("/newPost", (req,res)=>{
